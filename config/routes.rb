a24e52ad3c3237c60d2638bde3bcdade067c3794
nkmess <junna.m_21@icloud.com>
2019-07-01 20:22:40 +0900

    Add routing for card

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "products#index"
   resources :users, only: [:new, :edit]
   resources :logout, only: [:index]
+  resources :card, only: [:index, :new, :create, :destroy]
 end
