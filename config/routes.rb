41b7265965dd623177f82204e45b66ce2078d074
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-07-05 19:39:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'changes/edit'
   root "products#index"
-  resources :users do
-    get 'change', on: :member
-  resources :users, only: [:new, :edit, :index]
-  resources :logout, only: [:index]
   resources :products do
     get 'confirm_purchace', on: :member
   end
+  resources :logout, only: [:index]
+  resources :users do
+    get 'change', on: :member
 end
 end
