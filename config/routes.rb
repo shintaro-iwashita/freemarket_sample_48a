e008814948942a86da21b34e0896bb31a10849bf
nkmess <junna.m_21@icloud.com>
2019-07-11 21:33:56 +0900

    Revert "remake logouts_controller.rb"
    This reverts commit c3bf91adf1a634c22cd2f0440216d1a6738fa7e0.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   get 'changes/edit'
   root "products#index"
   resources :users, only: [:new, :edit, :index]
-  resources :logouts, only: [:index]
+  resources :logout, only: [:index]
   resources :card, only: [:index, :new, :create, :destroy]
   resources :detail, only: [:index]
   resources :products do
     get 'confirm_purchace', on: :member
   end
+  resources :logout, only: [:index]
   resources :users do
     get 'change', on: :member
 end
