4df1eafa1497b2288211c3856c70b0413d7009a1
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-12 21:28:58 +0900

    ModifiedRoutes
    ModifiedRoutes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get 'confirm_purchace', on: :member
   end
   resources :logout, only: [:index]
+  resources :confirms, only: [:edit]
   resources :users do
     get 'change', on: :member
 end
