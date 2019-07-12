166f326e869bb51befd9908b096ad398ce0d989d
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-12 21:44:48 +0900

    a
    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root "products#index"
-  resources :users, only: [:new, :edit]
-  resources :logout, only: [:index]
-  resources :registrations, only: [:new]
   resources :products, only: [:index, :show]
   resources :users, only: [:index, :new, :edit,]
   resources :logouts, only: [:index]
   resources :cards, only: [:index, :new, :create, :destroy]
+  resources :registrations, only: [:new]
+  
   resources :products do
     get 'confirm_purchace', on: :member
   end
   resources :users do
     get 'change', on: :member
+end
 end
