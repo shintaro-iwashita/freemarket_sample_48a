4748d5e8121b528a6f90fb593e80f1821a455a45
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-12 20:44:37 +0900

    ModifiedController
    ModifiedController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "products#index"
-  get 'users/confirm' => 'users#confirm'
   resources :users, only: [:new, :edit]
   resources :logout, only: [:index]
+  resources :registrations, only: [:new]
 end
