b4e7304c9fe8d22d9f1a9a5dda70d443b7fc3069
nkmess <junna.m_21@icloud.com>
2019-07-13 12:44:14 +0900

    remake purchases_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root "products#index"
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :purchases, only: [:index]
+  end
   resources :users, only: [:index, :new, :edit,]
   resources :logouts, only: [:index]
   resources :cards, only: [:index, :new, :create, :destroy]
 
-  resources :products do
-    get 'confirm_purchace', on: :member
-  end
   resources :users do
     get 'change', on: :member
-end
+  end
 end
