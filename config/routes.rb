00fd16ca8110487c1a01120f5ef0ee5962612db0
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-13 18:10:47 +0900

    a
    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "products#index"
   resources :products, only: [:index, :show] do
     resources :purchases, only: [:index]
   end
-  resources :users, only: [:index, :new, :edit,]
   resources :logouts, only: [:index]
   resources :cards, only: [:index, :new, :create, :destroy]
 
