cea3bd0907be9ab49adda31818cb6d3ca012c99d
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-13 23:13:42 +0900

    あ
    あ

--- Gemfile
+++ Gemfile
@@ -71,6 +71,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'haml-rails'
+gem 'erb2haml'
 gem 'font-awesome-sass'
 gem 'payjp'
 gem "gretel"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users
-=======
-  devise_for :installs
->>>>>>> Stashed changes
   root "products#index"
   resources :products, only: [:index, :show] do
     resources :purchases, only: [:index]
