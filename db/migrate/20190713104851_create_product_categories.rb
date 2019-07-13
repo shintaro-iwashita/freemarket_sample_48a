be8eafa0613ee6f87f517f31e139ce89a2d3e915
hiroki189 <hiroki18950@icloud.com>
2019-07-13 20:06:09 +0900

    product_category_table

---
+++ db/migrate/20190713104851_create_product_categories.rb
@@ -0,0 +1,11 @@
+class CreateProductCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_categories do |t|
+      t.string    :name, null: false
+      t.references   :parent,   null: false
+      t.references   :product, null: false
+
+      t.timestamps
+    end
+  end
+end
