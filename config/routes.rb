0bc27414ace213f13cb12902ecf7ff31ed93d96c
nkmess <junna.m_21@icloud.com>
2019-07-14 18:32:37 +0900

    add routing for registration

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :edit,] do
     resources :confirms, only: [:edit]
   end
+
+  # 新規会員登録画面お届け先住所
+  resources :adresses, only: [:new, :create] 
+  # 新規会員登録完了画面
+  resources :completions, only: [:index]
+
   resources :logouts, only: [:index]
   resources :cards, only: [:index, :new, :create, :destroy]
   resources :registrations, only: [:new]
