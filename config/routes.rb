084a3642f82f9983c0a423f86b7b48b5e8e53188
nkmess <junna.m_21@icloud.com>
2019-07-14 12:31:16 +0900

    Revert "ChangedClass"
    This reverts commit ae9e7f4300051a4378fe4354349f6202ebd97950.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  root "products#index"
-  resources :cards, only: [:index, :new, :create, :destroy]
-  resources :registrations, only: [:new]
-
   resources :products do
     get 'confirm_purchace', on: :member
   end
