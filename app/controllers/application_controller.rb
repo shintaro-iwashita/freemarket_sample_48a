9279d4f3697f5d538a8ec0d783373836dd728902
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-17 19:17:24 +0900

    ligin
    ligin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  private
+  protected
 
   def production?
     Rails.env.production?
@@ -12,5 +13,12 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :user_name, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -290,7 +290,7 @@ Devise.setup do |config|
   # ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   # end
-
+  config.scoped_views = true
   # ==> Configuration for :registerable
 
   # When set to false, does not sign a user in automatically after their password is
