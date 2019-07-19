41f2d522680d8d01681311bc69a0c35c88dfd202
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-07-20 02:14:19 +0900

    Update breadcrumbs.rb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,42 +2,38 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-  # crumb :brands do
-  #   link "ブランド一覧", brands_path
-  #   parent :root
-  # end
-
-  # crumb :categories do
-  #   link "カテゴリー一覧", categories_path
-  #   parent :root
-  # end
-
-crumb :mypage do
-  link "マイページ", users_path(current_user)
+crumb :brands do
+  link "ブランド一覧", brands_path
   parent :root
 end
 
-# crumb :item do |item|
-#   link item.name, item_path(item)
-#   parent :root
-# end
+crumb :categories do
+  link "カテゴリー一覧", categories_path
+  parent :root
+end
+
+crumb :mypage do
+  link "マイページ", users_path
+  parent :root
+end
+
+crumb :product do
+  link "商品名", product_path
+  parent :root
+end
 
 crumb :profile do
-  link "プロフィール", edit_user_path
+  link "プロフィール編集", edit_user_path
   parent :mypage
 end
 
-crumb :userAddress do
+crumb :confirm do
   link "本人情報の登録", edit_user_confirm_path
   parent :mypage
 end
 
 crumb :logout do
-  link "ログアウト", logouts_path
+  link "ログアウト"
   parent :mypage
 end
 
-# crumb :category do |category|
-#   link category.name, category
-#   parent :categories
-# end
