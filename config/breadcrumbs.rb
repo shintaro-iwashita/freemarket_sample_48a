1c6424404ee8e91e137808ea481adefdd0b94237
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-07-19 09:04:32 +0900

    Update breadcrumbs.rb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,65 +2,42 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-# crumb :brands do
-#   link "ブランド一覧", brands_path
-# end
+  # crumb :brands do
+  #   link "ブランド一覧", brands_path
+  #   parent :root
+  # end
 
-# crumb :categories do
-#   link "カテゴリー一覧", categories_path
-# end
+  # crumb :categories do
+  #   link "カテゴリー一覧", categories_path
+  #   parent :root
+  # end
 
-# crumb :mypage do
-#   link "マイページ", user_path(current_user)
-# end
+crumb :mypage do
+  link "マイページ", users_path(current_user)
+  parent :root
+end
 
 # crumb :item do |item|
 #   link item.name, item_path(item)
+#   parent :root
 # end
 
-# crumb :profile do
-#   link "プロフィール", edit_user_path
-# end
+crumb :profile do
+  link "プロフィール", edit_user_path
+  parent :mypage
+end
 
-# crumb :userAddress do
-#   link "本人情報の登録", edit_address_path
-# end
+crumb :userAddress do
+  link "本人情報の登録", edit_user_confirm_path
+  parent :mypage
+end
 
-# crumb :logout do
-#   link "ログアウト"
-# end
+crumb :logout do
+  link "ログアウト", logouts_path
+  parent :mypage
+end
 
 # crumb :category do |category|
 #   link category.name, category
+#   parent :categories
 # end
-
-
-
-
-crumb :logout do
-  link "ログアウト", logout_index_path
-end
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
