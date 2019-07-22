93a4c18a657c8c208fb621e584da637b8e94ea25
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-23 00:23:42 +0900

    LoginByFacebook_omniauth.rb
    LoginByFacebook_omniauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['866168363764565'], ENV['1dee036317fc78be52176fc12a0a94c8'], info_fields: 'first_name'
+end
+# config.omniauth :facebook, '866168363764565', '1dee036317fc78be52176fc12a0a94c8'
+# , last_name, gender
