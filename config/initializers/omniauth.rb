5e631462ea068369f549bdc5069489c05a535d39
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-07-23 22:47:18 +0900

    chaged
    chaged

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['866168363764565'], ENV['1dee036317fc78be52176fc12a0a94c8'], info_fields: 'first_name, last_name, gender'
-  
-end
-# config.omniauth :facebook, '866168363764565', '1dee036317fc78be52176fc12a0a94c8'
-# , last_name, gender
