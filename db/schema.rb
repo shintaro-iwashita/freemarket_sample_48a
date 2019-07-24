d0b75accc6c9ea9b71cf4dec9c00afd508a95614
nkmess <junna.m_21@icloud.com>
2019-07-24 20:14:25 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_124939) do
+ActiveRecord::Schema.define(version: 2019_07_22_102901) do
 
   create_table "product_brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_07_15_124939) do
     t.string "name", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_product_categories_on_ancestry"
   end
 
   create_table "product_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
