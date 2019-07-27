ad48d3b777ec8b53f01b8da06bd2ae247956ffb6
hiroki189 <hiroki18950@icloud.com>
2019-07-27 16:07:43 +0900

    change_rb

--- db/migrate/20190715082759_create_products.rb
+++ db/migrate/20190715082759_create_products.rb
@@ -1,14 +1,23 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
-  def change
+  def chang
     create_table :products do |t|
       t.string :name, null: false, length: { maximum: 40 }
-      t.integer :price, null: false
-      t.text :pescription, null: false, length: { maximum: 1000 }
-      t.integer :profit, null:false
-      t.integer :size, null: false
-      t.integer :condition, null: false
+      t.text :description, null: false, length: { maximum: 1000 }
       t.references :user, null: false, foreign_key: true
+      t.integer :size_id
+      t.integer :condition_id, null: false
+      t.integer :price, null: false
+      t.integer :profit, null:false
+      t.references :category, foreign_key: true
+      t.integer :delivery_responsibility, null: false
+      t.integer :delivery_method, null: false
+      t.integer :delivery_area, null: false
+      t.integer :delivery_day, null: false
+      t.references :seller, foreign_key: true
+      t.references :integer, foreign_key: true
       t.timestamps null: true
     end
   end
 end
+
+forein_key
