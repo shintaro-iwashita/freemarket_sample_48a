7425a068785318a6348f72709aebef005ca81b14
hiroki189 <hiroki18950@icloud.com>
2019-07-30 16:31:48 +0900

    create_category-controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
   def index
     @product_categories = ProductCategory.all.order("ancestry ASC").limit(13)
-    ProductCategory.find(1).children
   end
 end
