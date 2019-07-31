dc000fb75286d3ba73251554fba173a228dbb2c4
hiroki189 <hiroki18950@icloud.com>
2019-07-31 14:47:16 +0900

    modify

--- app/assets/javascripts/category-index.js
+++ app/assets/javascripts/category-index.js
@@ -3,7 +3,7 @@ $(function(){
     var speed = 500;
     var href = $(this).attr("href");
     var postion = $("#root_category-1").offset().top;
-    $("html,body").animate({scrollTop: 400});
+    $("html,body").animate({scrollTop: 450});
     return false;
   });
 });
