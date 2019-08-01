1f0960ccc0c4dc402e317e7bc694884d770d6563
nkmess <junna.m_21@icloud.com>
2019-08-01 15:20:37 +0900

    Create some data with enum

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,11 @@ class Product < ApplicationRecord
 
   enum condition_id: [:unused, :like_new, :invisibly_damaged, :slightly_damaged, :damaged, :bad]
 
-  enum shipping_responsibility: [:including_postage, :cash_on_delivery]
+  enum delivery_responsibility: [:including_postage, :cash_on_delivery]
 
-  enum shipping_method: [:undecided, :easy_mercari, :yu_mail, :letter_pack, :regular_mail, :kuronekoyamato, :yu_pack, :click_post, :yu_pakcet]
+  enum delivery_method: [:undecided, :easy_mercari, :yu_mail, :letter_pack, :regular_mail, :kuronekoyamato, :yu_pack, :click_post, :yu_pakcet]
 
-  enum shipping_day: [:in_two_days, :in_three_days, :in_seven_days]
+  enum delivery_day: [:in_two_days, :in_three_days, :in_seven_days]
 
 
   belongs_to_active_hash :prefecture
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module FreemarketSample48a
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
