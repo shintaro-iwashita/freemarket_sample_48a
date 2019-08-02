8bbb646dd469eb3edfb918bfea810f0e6997466b
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-08-03 00:23:09 +0900

    Update breadcrumbs.rb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,13 +2,19 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-crumb :brands do
-  link "ブランド一覧", brands_path
-  parent :root
+# カテゴリ個別ページ実装後にビューに記述
+crumb :category do |category|
+  link category.name, category
+  parent :categories
 end
 
 crumb :categories do
   link "カテゴリー一覧", categories_path
+  parent :root
+end
+
+crumb :brands do
+  link "ブランド一覧", brands_path
   parent :root
 end
 
@@ -17,6 +23,13 @@ crumb :mypage do
   parent :root
 end
 
+# 商品個別ページ実装後にビューに記述
+crumb :product do |product|
+  link product.name, product_path(product)
+  parent :root
+end
+
+# 商品名仮置き
 crumb :product do
   link "商品名", product_path
   parent :root
