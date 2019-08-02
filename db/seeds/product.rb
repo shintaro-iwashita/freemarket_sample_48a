82e4c68872febf0648ab6e1a9ffbf1589171b2a7
hiroki189 <hiroki18950@icloud.com>
2019-08-02 18:00:15 +0900

    product modify

--- db/seeds/product.rb
+++ db/seeds/product.rb
@@ -1,30 +1,39 @@
-Product.create!(
-  [
-  {
-  name:"チキン",
-  description: "使用感あり",
-  size_id: "1",
-  condition_id: "1",
-  price: "10",
-  profit: "1",
-  product_category_id: 1,
-  delivery_responsibility: "1",
-  delivery_method: "1",
-  delivery_area:"1",
-  delivery_day:"1",
-  },
-  {
-  name:"ライス",
-  description: "使用感あり",
-  size_id: "2",
-  condition_id: "2",
-  price: "12",
-  profit: "2",
-  product_category_id: 2,
-  delivery_responsibility: "2",
-  delivery_method: "2",
-  delivery_area:"2",
-  delivery_day:"2",
-  },
-  ]
-)
+10.times do |i|
+  Product.create!(
+    [
+    {
+    name:"#{i}",
+    description: "#{i}",
+    size_id: "#{i}",
+    condition_id: "#{i}",
+    price: "#{i}",
+    profit: "#{i}",
+    product_category_id: "1/#{i}",
+    delivery_responsibility: "#{i}",
+    delivery_method: "#{i}",
+    delivery_area:"#{i}",
+    delivery_day:"#{i}",
+    }
+    ]
+  )
+end
+
+10.times do |i|
+  Product.create!(
+    [
+    {
+    name:"#{i}",
+    description: "#{i}",
+    size_id: "#{i}",
+    condition_id: "#{i}",
+    price: "#{i}",
+    profit: "#{i}",
+    product_category_id: "219/#{i}",
+    delivery_responsibility: "#{i}",
+    delivery_method: "#{i}",
+    delivery_area:"#{i}",
+    delivery_day:"#{i}",
+    }
+    ]
+  )
+end
