00fadcbfad274f0b3b9cf0f7842502079db95f47
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-08-03 17:35:41 +0900

    Update product_image.rb

--- db/seeds/product_image.rb
+++ db/seeds/product_image.rb
@@ -1,7 +1,7 @@
 4.times do |index|
   ProductImage.create!(
     id: "#{index}",
-    image: "sample#{index}.jpeg",
+    image: "https://images.pexels.com/photos/12211/pexels-photo-12211.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     product_id: "#{index}",
     created_at: '2019-01-01', 
     updated_at: '2019-01-01',
