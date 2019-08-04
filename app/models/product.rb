30c17f6c237babed3a44a064fe65cebcdf51f7aa
haru6532 <k7pgn8nebpu6c@outlook.jp>
2019-08-04 17:42:52 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :product_category, optional: true
   has_many :product_images, dependent: :destroy
+  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id'
+  belongs_to :seller, class_name: 'User', :foreign_key => 'seller_id'
 end
