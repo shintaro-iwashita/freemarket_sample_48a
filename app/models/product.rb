ba73896b6e61f034714990347938f779bdd06eb4
nkmess <junna.m_21@icloud.com>
2019-08-05 08:54:07 +0900

    add option to buyer_id

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,6 +13,6 @@ class Product < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :product_category, optional: true
   has_many :product_images, dependent: :destroy
-  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id'
+  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id', optional: true
   belongs_to :seller, class_name: 'User', :foreign_key => 'seller_id'
 end
