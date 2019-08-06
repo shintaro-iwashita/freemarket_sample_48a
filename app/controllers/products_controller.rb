7ebeca7cb8dd9fb6964bc3ea2f3a27bd4a059169
hiroki189 <hiroki18950@icloud.com>
2019-08-06 17:47:49 +0900

    products-controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,31 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @products = Product.find(params[:id])
+    @product_images = ProductImage.find(params[:id])
+    binding.pry
   end
 
   def new
+    @product = Product.new
+    @category_parent_array = ProductCategory.where(ancestry: nil)
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to controller: :products, action: :index
+  end
+
+  def get_category_children
+    @category_children = ProductCategory.find_by(id: "#{params[:parent_id]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = ProductCategory.find("#{params[:child_id]}").children
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :size_id, :condition_id, :price, :product_category_id, :delivery_responsibility, :delivery_method, :delivery_area, :delivery_day).merge(seller_id: current_user.id)
   end
 
 end
