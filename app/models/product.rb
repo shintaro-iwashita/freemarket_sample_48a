d7cf1130fdb0fc1e2a695f7c1d9dbe0551b726a0
hiroki189 <hiroki18950@icloud.com>
2019-08-06 17:48:51 +0900

    putting-up information

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  enum condition_id: [:unused, :like_new, :invisibly_damaged, :slightly_damaged, :damaged, :bad]
+
+  enum delivery_responsibility: [:including_postage, :cash_on_delivery]
+
+  enum delivery_method: [:undecided, :easy_mercari, :yu_mail, :letter_pack, :regular_mail, :kuronekoyamato, :yu_pack, :click_post, :yu_pakcet]
+
+  enum delivery_day: [:in_two_days, :in_three_days, :in_seven_days]
+
+
+  belongs_to_active_hash :prefecture
   belongs_to :product_category, optional: true
   has_many :product_images, dependent: :destroy
   belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id'
