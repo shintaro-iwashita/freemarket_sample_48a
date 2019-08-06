0831523533215f04d846eb7a8d605cca414ab728
hiroki189 <hiroki18950@icloud.com>
2019-08-06 10:50:35 +0900

    comment_out

--- db/migrate/20190804085809_remove_profit_from_products.rb
+++ db/migrate/20190804085809_remove_profit_from_products.rb
@@ -1,5 +1,5 @@
-class RemoveProfitFromProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :products, :profit, :integer
-  end
-end
+# class RemoveProfitFromProducts < ActiveRecord::Migration[5.2]
+#   def change
+#     remove_column :products, :profit, :integer
+#   end
+# end
