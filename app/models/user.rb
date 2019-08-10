ce0f3d46d7c3f3307d5853c37be3dec054156d65
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-08-10 14:37:18 +0900

    a
    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,:omniauthable,
          omniauth_providers: [:facebook, :google_oauth2]
 
-  validates :nickname,presence: true;
-
+  validates :nickname, presence: true, length: { maximum: 6 }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
+
     it "is valid with a nickname, email, password, password_confirmation" do
-      user = build(:user)
+      user = FactoryBot.create(:user)
       expect(user).to be_valid
     end
 
@@ -12,36 +14,6 @@ describe User do
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
-    it "is invalid without a email" do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
-    end
-
-    it "is invalid without a password" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
-    end
-
-    it "is invalid without a password_confirmation although with a password" do
-      user = build(:user, password_confirmation: "")
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-    end
-
-    it "is invalid with a password that has less than 5 characters " do
-      user = build(:user, password: "00000", password_confirmation: "00000")
-      user.valid?
-      expect(user.errors[:password][0]).to include("is too short")
-    end
-
-    it "is invalid with a duplicate email address" do
-      user = create(:user)
-      another_user = build(:user)
-      another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
-    end
 
   end
 end
