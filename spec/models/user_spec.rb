71754f795b005ca688b623ac7661c34b681f7817
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-08-10 14:52:47 +0900

    Japaneselize
    Japaneselize

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,46 @@
 require 'rails_helper'
-
 describe User do
   describe '#create' do
-
-    it "is valid with a nickname, email, password, password_confirmation" do
-      user = FactoryBot.create(:user)
+    it "is valid with a nickname, email, password, password_confirmation,family_name,family_name_kana,first_name,first_name_kana" do
+      user = build(:user)
       expect(user).to be_valid
     end
 
     it "is invalid without a nickname" do
       user = build(:user, nickname: "")
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user.errors[:nickname]).to include("を入力してください")
+    end
+
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "is invalid without a password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
+    end
+
+    it "is invalid with a password that has less than 5 characters " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password][0]).to include("は6文字以上で入力してください")
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
 
