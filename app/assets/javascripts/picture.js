8d74bb7f1e81b2aee78964c1f3ea87630f4e58ea
nkmess <junna.m_21@icloud.com>
2019-08-10 14:03:01 +0900

    Update view for image

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -2,17 +2,24 @@ $(document).on('turbolinks:load', function() {
 
   $(function(){
     $('#pic_upload').change(function(e){
-      var files = e.target.files;
-      console.log(files)
+      var files = e.target.files[0];
+      var reader = new FileReader();
+      $preview = $(".preview");
+      t = this;
     })
 
-  })
+    reader.onload = (function(files){
+      return function(e){
+      $preview.append($('img')).attr({
+        src: e.target.result,
+        width: "150px",
+        class: "preview",
+        title: file.name
+        });
+      };
+    })(files);
 
+    reader.readAsDataURL(files);
+  });
 
-
-
-
-
-
-
-})
+});
