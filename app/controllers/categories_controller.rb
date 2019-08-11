d68a7dc3595189406eb0e4a307c4b8a5c2972cc2
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-08-11 14:56:25 +0900

    list
    list

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,15 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @images = []
+    @products = Product.where(product_category_id:params[:id])
+    @products.each do |p|
+      @images << ProductImage.find(p.id)
+    end
+    @category = ProductCategory.find(params[:id])
+  end
+
+  def show2
     # // あとでproduct_categoriesのID
     @products = Product.order("id ASC").limit(100)
   end
