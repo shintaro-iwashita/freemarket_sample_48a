3391f0e305af7f122338e6e25eaa5920085919d7
nkmess <junna.m_21@icloud.com>
2019-08-11 12:46:34 +0900

    Create preview function

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,21 +1,31 @@
 $(document).on('turbolinks:load', function() { 
 
   $(function(){
+
+    function buildImgHtml(blobUrl){
+      var image = `<img src="${blobUrl}" id="preview-image">`;
+      return image;
+    }
+
+    function appendImgBox(insertImg){
+      var imagePreviewHtlm = '';
+      imagePreviewHtlm = `<div class="upload__image">
+                            ${insertImg}
+                            <div class="upload__image__btn">
+                              <a class="remove-btn">削除</a>
+                          </div>`
+      $(imagePreviewHtlm).insertAfter('.upload__images');
+    }
+
+
     $('#pic_upload').change(function(e){
       var files = e.target.files[0];
       var blobUrl = window.URL.createObjectURL(files);
 
-      var img = $(`<div class="upload__image">
-                    <img id="preview-image">
-                    <div class="upload__image__btn">
-                      <a class="remove-btn">削除</a>
-                  </div> 
-                </li>`);
-      
-      $('#preview-image').attr('src', blobUrl);
-    })
-
-
+      insertImg = buildImgHtml(blobUrl);
+      appendImgBox(insertImg);
+    });
+    
+  
   });
-
 });
