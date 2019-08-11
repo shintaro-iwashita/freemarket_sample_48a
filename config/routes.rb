0f648f825cc0ebd194cfc5520c660b4ceea95877
hiroki189 <hiroki18950@icloud.com>
2019-08-11 15:56:39 +0900

    sign function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+  registrations: 'users/registrations',sessions: 'users/sessions'}
   root "products#index"
   resources :products, only: [:index, :show, :new, :create] do
     resources :purchases, only: [:index]
