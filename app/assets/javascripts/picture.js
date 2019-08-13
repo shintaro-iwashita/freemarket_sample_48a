642ad89ad63623856355b8d2b043f72f9f80cc65
nkmess <nkmess12@gmail.com>
2019-08-13 09:44:16 +0900

    Remove unnecessary code

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,63 +1,61 @@
-$(document).on('turbolinks:load', function() { 
+$(function(){
+  var itemNum = 1;
+  // 画像URLをセッティング
+  function buildImgHtml(blobUrl){
+    var image = `<img src="${blobUrl}" id="preview-image">`;
+    return image;
+  }
 
-  $(function(){
-    var itemNum = 1;
-    // 画像URLをセッティング
-    function buildImgHtml(blobUrl){
-      var image = `<img src="${blobUrl}" id="preview-image">`;
-      return image;
+  // 画像+ボタンのひとかたまりを作る
+  function appendImgBox(insertImg){
+    var imagePreviewHtlm = '';
+    if (itemNum <= 4) {
+      imagePreviewHtlm = `<div class="upload__image">
+                            ${insertImg}
+                            <div class="upload__image__btn">
+                              <p class="remove-btn">削除</a></div></div>
+                          <input multiple= "multiple" name="product[images][]" class="upload-image" data-image= "${itemNum}" type="file" id="pic_upload" style= "display: none;">
+                          <div class="sell-upload-drop-box" onclick= "$('[data-image= ${itemNum}]').click()"></div>`
+      $(imagePreviewHtlm).appendTo('#firstbox');
+    } else if (itemNum == 5) {
+      imagePreviewHtlm = `<div class="upload__image">
+                            ${insertImg}
+                            <div class="upload__image__btn">
+                              <p class="remove-btn">削除</a></div>
+                          </div>`
+      newDropBox = `<input multiple= "multiple" name="product[images][]" class="upload-image" 
+      data-image="${itemNum}" type="file" id="pic_upload" style= "display: none">
+                    <div class="sell-upload-drop-box" onclick= "$('[data-image= ${itemNum}]').click()"></div>`
+      $(imagePreviewHtlm).appendTo('#firstbox');
+      $(newDropBox).appendTo('#secondbox');
+    } else if (itemNum < 10) {
+      imagePreviewHtlm = `<div class="upload__image">
+                            ${insertImg}
+                            <div class="upload__image__btn">
+                              <p class="remove-btn">削除</a></div></div>
+                          <input multiple= "multiple" name="product[images][]" class="upload-image" data-image="${itemNum}" type="file" id="pic_upload" style= "display: none;">
+                          <div class="sell-upload-drop-box" onclick= "$('[data-image= ${itemNum}]').click()"></div>`
+    $(imagePreviewHtlm).appendTo('#secondbox');
+    } else {
+      imagePreviewHtlm = `<div class="upload__image">
+                            ${insertImg}
+                            <div class="upload__image__btn">
+                              <p class="remove-btn">削除</a></div></div>`
+    $(imagePreviewHtlm).appendTo('#secondbox');
     }
+    itemNum += 1;
+  }
 
-    // 画像+ボタンのひとかたまりを作る
-    function appendImgBox(insertImg){
-      var imagePreviewHtlm = '';
-      if (itemNum <= 4) {
-        imagePreviewHtlm = `<div class="upload__image">
-                              ${insertImg}
-                              <div class="upload__image__btn">
-                                <p class="remove-btn">削除</a></div></div>
-                            <input multiple= "multiple" name="product[images][]" class="upload-image" data-image= "${itemNum}" type="file" id="pic_upload" style= "display: none;">
-                            <div class="sell-upload-drop-box" onclick= "$('[data-image= ${itemNum}]').click()"></div>`
-        $(imagePreviewHtlm).appendTo('#firstbox');
-      } else if (itemNum == 5) {
-        imagePreviewHtlm = `<div class="upload__image">
-                              ${insertImg}
-                              <div class="upload__image__btn">
-                                <p class="remove-btn">削除</a></div>
-                            </div>`
-        newDropBox = `<input multiple= "multiple" name="product[images][]" class="upload-image" 
-        data-image="${itemNum}" type="file" id="pic_upload" style= "display: none">
-                      <div class="sell-upload-drop-box" onclick= "$('[data-image= ${itemNum}]').click()"></div>`
-        $(imagePreviewHtlm).appendTo('#firstbox');
-        $(newDropBox).appendTo('#secondbox');
-      } else if (itemNum < 10) {
-        imagePreviewHtlm = `<div class="upload__image">
-                              ${insertImg}
-                              <div class="upload__image__btn">
-                                <p class="remove-btn">削除</a></div></div>
-                            <input multiple= "multiple" name="product[images][]" class="upload-image" data-image="${itemNum}" type="file" id="pic_upload" style= "display: none;">
-                            <div class="sell-upload-drop-box" onclick= "$('[data-image= ${itemNum}]').click()"></div>`
-      $(imagePreviewHtlm).appendTo('#secondbox');
-      } else {
-        imagePreviewHtlm = `<div class="upload__image">
-                              ${insertImg}
-                              <div class="upload__image__btn">
-                                <p class="remove-btn">削除</a></div></div>`
-      $(imagePreviewHtlm).appendTo('#secondbox');
-      }
-      itemNum += 1;
-    }
+  $(document).on("change", "#pic_upload",function(e){
+    var files = e.target.files[0];
+    var blobUrl = window.URL.createObjectURL(files);
 
-    $(document).on("change", "#pic_upload",function(e){
-      var files = e.target.files[0];
-      var blobUrl = window.URL.createObjectURL(files);
-
-      insertImg = buildImgHtml(blobUrl);
-      $('.sell-upload-drop-box').css('display', 'none');
-      appendImgBox(insertImg);
-
-    });
-
+    insertImg = buildImgHtml(blobUrl);
+    $('.sell-upload-drop-box').css('display', 'none');
+    appendImgBox(insertImg);
 
   });
+
+
 });
+
