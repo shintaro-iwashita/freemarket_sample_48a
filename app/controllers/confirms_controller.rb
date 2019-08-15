39e597e5a32d17b5ca6c77e267264cb31f615b75
nkmess <nkmess12@gmail.com>
2019-08-15 19:43:38 +0900

    Setting before_action :authenticate_user!

--- app/controllers/confirms_controller.rb
+++ app/controllers/confirms_controller.rb
@@ -1,4 +1,5 @@
 class ConfirmsController < ApplicationController
+  before_action :authenticate_user!
 
   def edit
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:new]
+
   def index
   end
   
