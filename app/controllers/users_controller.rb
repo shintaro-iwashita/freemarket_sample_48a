3131b844513f56ad457e0f375e4cdf8b8d87f135
YutaYam <navysealsdeltaforce@yahoo.co.jo>
2019-08-15 17:44:56 +0900

    link
    link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  before_action :move_to_index,except:[:new]
   def index
+
   end
   
   def new
+
   end
   
+  def move_to_index
+    redirect_to action: :new unless user_signed_in?
+  end
 end
--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -3,6 +3,7 @@ users= [
     email: "aaa@aaa", 
     password: "aaaaaa",
     nickname: "aaa",
+    avatar:"http://img.g-x.jp/articles/4d9ae101-1040-4aa5-ad50-0ce3caac1ca2/img/o/%83A%83o%83%5E%81%5B02.jpg",
     family_name: "aaa",
     first_name: "aaa",
     family_name_kana: "aaa",
