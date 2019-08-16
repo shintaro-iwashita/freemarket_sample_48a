857593d203d02d328c013f85ea06fe9378545b69
hiroki189 <hiroki18950@icloud.com>
2019-08-16 16:51:41 +0900

    delete recapcha

--- app/controllers/logouts_controller.rb
+++ app/controllers/logouts_controller.rb
@@ -1,6 +1,10 @@
 class LogoutsController < ApplicationController
-
+  before_action :move_to_index
   def index
   end
-  
+
+  def move_to_index
+    redirect_to root_path unless user_sign_in?
+  end
+
 end
