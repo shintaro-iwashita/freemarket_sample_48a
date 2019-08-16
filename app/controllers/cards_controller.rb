8cd44e75359484035935d5aa7bbd079286b6ffd5
shintaro-iwashita <shintaroiwashita7857@gmail.com>
2019-08-16 21:56:21 +0900

    file chenge

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,15 +7,13 @@ class CardsController < ApplicationController
   def create #PayjpとCardのデータベースを作成
     respond_to do |format|
       format.json {
-        # require 'payjp'
         Payjp.api_key = ENV["PAYJP_SECRET_ACCESS_KEY"]  
-        # current_user.update(token_id: params[:token])
-        if current_user.creditCards == []
+          current_user.creditCards == []
           response_customer = Payjp::Customer.create(card: params[:token])
           current_user.creditCards.create(token_id: response_customer.id, user_id: current_user.id)
-        end
       }
     end
+  end
 
   def destroy
   end
