9ec425406ae675551a2f9ee7130ef2a8c7bc168e
shintaro-iwashita <shintaroiwashita7857@gmail.com>
2019-08-16 20:44:10 +0900

    delete file

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,8 @@
 class CardsController < ApplicationController
- 
-
 
   def new 
     @card = CreditCard.new
   end
-
 
   def create #PayjpとCardのデータベースを作成
     respond_to do |format|
@@ -19,32 +16,6 @@ class CardsController < ApplicationController
         end
       }
     end
-
-    # if params['payjp-token'].blank?
-    #   redirect_to action: "new"
-    # else
-    #   # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
-    #   customer = Payjp::Customer.create(
-    #     description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
-    #     email: current_user.email,
-    #     card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
-    #     metadata: {user_id: current_user.id} # 無くてもOK。
-    #   )
-    #   @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-    #   if @card.save
-    #     redirect_to action: "index"
-    #   else
-    #     redirect_to action: "create"
-    #   end
-    # end
-  end
-
-  private
-
-  # def set_card
-  #   @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
-  # end
-
 
   def destroy
   end
