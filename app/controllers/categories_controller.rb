072ad187ced9b019777ff405b0fe79aa021f3e8a
hiroki189 <hiroki18950@icloud.com>
2019-08-17 19:07:56 +0900

    modify

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
   def index
     @product_categories = ProductCategory.order("ancestry ASC").limit(13)
-    @products = ProductCategory.find(params[:id]).products
   end
 end
