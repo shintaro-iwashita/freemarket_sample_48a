fa2d297d41c1b63d70aa6d6bb3a0ad761219c03d
hiroki189 <hiroki18950@icloud.com>
2019-08-20 19:07:09 +0900

    modify

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :search]
   before_action :set_product, except:[:index, :new, :create, :show, :search,:get_category_children, :get_category_grandchildren]
   
 
