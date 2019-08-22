813ffefb9e33623c70fcdb379239c6f078c13a50
nkmess <nkmess12@gmail.com>
2019-08-22 10:50:39 +0900

    Change speed for slide

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,18 +1,18 @@
 $(function(){
 
   $('#arrow-left').click(function(){
-    $('.contents__top__image__box').animate({left: 0},"fast");
+    $('.contents__top__image__box').animate({left: 0}, 800);
   })
   $('#arrow-right').click(function(){
     screenWidth = $(window).width();
-    $('.contents__top__image__box').animate({left: -screenWidth},"fast").css('overflow', 'visible');
+    $('.contents__top__image__box').animate({left: -screenWidth}, 800).css('overflow', 'visible');
   })
 
 
   $('#dot-first').hover(
     function() {
       $(this).css('color', '#757575');
-      $('.contents__top__image__box').animate({left: 0},"fast");
+      $('.contents__top__image__box').animate({left: 0}, 800);
     },
     function() {
       $(this).css('color', 'white');
@@ -22,7 +22,7 @@ $(function(){
     function() {
       $(this).css('color', '#757575');
       screenWidth = $(window).width();
-      $('.contents__top__image__box').animate({left: -screenWidth},"fast").css('overflow', 'visible');
+      $('.contents__top__image__box').animate({left: -screenWidth}, 800).css('overflow', 'visible');
     },
     function() {
       $(this).css('color', 'white');
